e4f3dc1f6dcdf4668eeb1c9e1cfbbf5a705814a7
Lucas <lucasmaranho21@gmail.com>
2026-03-23 10:27:41 +0000

    Security added

--- FoodSafetyInspectionTracker/Controllers/PremisesController.cs
+++ FoodSafetyInspectionTracker/Controllers/PremisesController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FoodSafetyInspectionTracker.Data;
 using FoodSafetyInspectionTracker.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FoodSafetyInspectionTracker.Controllers
 {
+    [Authorize(Roles = "Admin,Inspector,Viewer")]
     public class PremisesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -19,13 +21,12 @@ namespace FoodSafetyInspectionTracker.Controllers
             _context = context;
         }
 
-        // GET: Premises
+       
         public async Task<IActionResult> Index()
         {
             return View(await _context.Premises.ToListAsync());
         }
 
-        // GET: Premises/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -43,15 +44,13 @@ namespace FoodSafetyInspectionTracker.Controllers
             return View(premises);
         }
 
-        // GET: Premises/Create
+       
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Premises/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Address,Town,RiskRating")] Premises premises)
@@ -65,7 +64,7 @@ namespace FoodSafetyInspectionTracker.Controllers
             return View(premises);
         }
 
-        // GET: Premises/Edit/5
+      
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,9 +80,7 @@ namespace FoodSafetyInspectionTracker.Controllers
             return View(premises);
         }
 
-        // POST: Premises/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+      
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,Town,RiskRating")] Premises premises)
@@ -116,7 +113,7 @@ namespace FoodSafetyInspectionTracker.Controllers
             return View(premises);
         }
 
-        // GET: Premises/Delete/5
+        
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -134,7 +131,7 @@ namespace FoodSafetyInspectionTracker.Controllers
             return View(premises);
         }
 
-        // POST: Premises/Delete/5
+       
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
